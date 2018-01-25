42329290150899ca2a469ed616786e6c409441cb
Conor Maguire <conormag94@gmail.com>
2018-01-25 22:30:38 +0000

    Add BLE advertising on button press

--- app/src/main/java/com/maguire/conor/attendanceapp/MainActivity.java
+++ app/src/main/java/com/maguire/conor/attendanceapp/MainActivity.java
@@ -1,16 +1,26 @@
 package com.maguire.conor.attendanceapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import java.nio.charset.Charset;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
     // This is an extra: Key-Value data that can be transferred from one intent to another
     public static final String EXTRA_MESSAGE = "com.maguire.conor.attendanceapp.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +38,35 @@ public class MainActivity extends AppCompatActivity {
 
     public void advertise(View view) {
         Log.i("BLE_ADVERTISE", "Advertising");
+
+        BluetoothLeAdvertiser advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode( AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY )
+                .setTxPowerLevel( AdvertiseSettings.ADVERTISE_TX_POWER_HIGH )
+                .setConnectable( false )
+                .build();
+
+        ParcelUuid pUuid = new ParcelUuid(UUID.fromString("EB342F19-99A4-4155-97CD-D3BFBF9E574B"));
+
+        AdvertiseData data = new AdvertiseData.Builder()
+                .setIncludeDeviceName( false )
+                .addServiceData( pUuid, "Data".getBytes( Charset.forName( "UTF-8" ) ) )
+                .build();
+
+        AdvertiseCallback advertisingCallback = new AdvertiseCallback() {
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                super.onStartSuccess(settingsInEffect);
+            }
+
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.e( "BLE", "Advertising onStartFailure: " + errorCode );
+                super.onStartFailure(errorCode);
+            }
+        };
+
+        advertiser.startAdvertising( settings, data, advertisingCallback );
     }
 }
