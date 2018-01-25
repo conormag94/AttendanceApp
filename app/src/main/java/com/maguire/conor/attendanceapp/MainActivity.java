068545f5eac88b59b30bb959d1226d3a1d45948c
Conor Maguire <conormag94@gmail.com>
2018-01-25 19:42:47 +0000

    Added transition to second activity and advertise button

--- app/src/main/java/com/maguire/conor/attendanceapp/MainActivity.java
+++ app/src/main/java/com/maguire/conor/attendanceapp/MainActivity.java
@@ -1,13 +1,32 @@
 package com.maguire.conor.attendanceapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // This is an extra: Key-Value data that can be transferred from one intent to another
+    public static final String EXTRA_MESSAGE = "com.maguire.conor.attendanceapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void advertise(View view) {
+        Log.i("BLE_ADVERTISE", "Advertising");
     }
 }
