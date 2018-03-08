b5e69e29b6bf8b25e36eb2e78338da8dcd62a5ee
Conor Maguire <conormag94@gmail.com>
2018-03-08 13:00:22 +0000

    Remove old unused advertise method from MainActivity

--- app/src/main/java/com/maguire/conor/attendanceapp/MainActivity.java
+++ app/src/main/java/com/maguire/conor/attendanceapp/MainActivity.java
@@ -1,21 +1,11 @@
 package com.maguire.conor.attendanceapp;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.le.AdvertiseCallback;
-import android.bluetooth.le.AdvertiseData;
-import android.bluetooth.le.AdvertiseSettings;
-import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.nio.charset.Charset;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -69,45 +59,4 @@ public class MainActivity extends AppCompatActivity {
         advertisingStatus.setText("Not Currently Advertising");
     }
 
-    public void advertise(View view) {
-        Log.i("BLE_ADVERTISE", "Advertising");
-
-        BluetoothLeAdvertiser advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
-
-//        BluetoothAdapter.getDefaultAdapter().setName("Mag");
-
-        AdvertiseSettings settings = new AdvertiseSettings.Builder()
-                .setAdvertiseMode( AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY )
-                .setTxPowerLevel( AdvertiseSettings.ADVERTISE_TX_POWER_HIGH )
-                .setConnectable( false )
-                .build();
-
-        ParcelUuid pUuid = new ParcelUuid(UUID.fromString("EB342F19-99A4-4155-97CD-D3BFBF9E574B"));
-
-        AdvertiseData data = new AdvertiseData.Builder()
-                .setIncludeDeviceName( true )
-                .addServiceData( pUuid, "Data".getBytes( Charset.forName( "UTF-8" ) ) )
-                .build();
-
-//        AdvertiseData data = new AdvertiseData.Builder()
-//                .setIncludeDeviceName( true )
-//                .addServiceUuid( pUuid )
-//                .addServiceData( pUuid, "Data".getBytes( Charset.forName( "UTF-8" ) ) )
-//                .build();
-
-        AdvertiseCallback advertisingCallback = new AdvertiseCallback() {
-            @Override
-            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
-                super.onStartSuccess(settingsInEffect);
-            }
-
-            @Override
-            public void onStartFailure(int errorCode) {
-                Log.e( "BLE", "Advertising onStartFailure: " + errorCode );
-                super.onStartFailure(errorCode);
-            }
-        };
-
-        advertiser.startAdvertising( settings, data, advertisingCallback );
-    }
 }
